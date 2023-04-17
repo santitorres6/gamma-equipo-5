629df58d52084e607f6552ed556aa51282c1bf09
maria <malejandravallejop@gmail.com>
2023-04-17 13:20:39 -0500

    eliminar card

--- filantropia-front-end/src/App.jsx
+++ filantropia-front-end/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.scss";
-import { Card } from "./components/card/Card";
 import { Header } from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Card   />
       
     </div>
   );
