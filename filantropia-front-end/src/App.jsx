efd6d4c15dc0bf73897906f8ff248bd386155560
maria <malejandravallejop@gmail.com>
2023-04-17 11:49:13 -0500

    cards

--- filantropia-front-end/src/App.jsx
+++ filantropia-front-end/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.scss";
+import { Card } from "./components/card/Card";
 import { Header } from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card   />
+      
     </div>
   );
 }
